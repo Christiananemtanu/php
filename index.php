a8185e5e66b917f7d6b2d1ce0a66066356207ba0
Christiananemtanu <christiana.nemtanu@gmail.com>
2022-02-16 21:58:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,20 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+   
 </head>
 <body>
     <img src="php/kuvia/kuva1.jpeg">
     <img src="php/kuvia/kuva2.jpeg">
     <img src="php/kuvia/kuva3.jpeg">
     <img src="php/kuvia/kuva4.jpeg">
-
+    
     <?php
-        $kuva[] = "kuva1";
-        $kuva[] = "kuva2";
-        $kuva[] = "kuva3";
-        $kuva[] = "kuva4";
 
-    echo $kuva[rand(0,3)];
+        $kuva[] = "kuva1.jpeng";
+        $kuva[] = "kuva2.jpeng";
+        $kuva[] = "kuva3.jpeng";
+        $kuva[] = "kuva4.jpeng";
+
+        echo $kuva[rand(0,3)];
+
+
         ?>
     
 </body>
